61ec43897e19e61ae3c83d7012118ecf79c13db2
Gabriel <gabrielkadaaj@yahoo.com.br>
2026-07-31 18:01:00 -0300

    feat: adiciona consulta de sumario de vendas

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,7 +1,9 @@
 package com.devsuperior.dsmeta.repositories;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -19,5 +21,15 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
             LocalDate maxDate,
             String name,
             Pageable pageable
+    );
+
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleSummaryDTO(obj.seller.name, SUM(obj.amount)) "
+            + "FROM Sale obj "
+            + "WHERE obj.date BETWEEN :minDate AND :maxDate "
+            + "GROUP BY obj.seller.name "
+            + "ORDER BY SUM(obj.amount) DESC")
+    List<SaleSummaryDTO> salesSummary(
+            LocalDate minDate,
+            LocalDate maxDate
     );
 }
