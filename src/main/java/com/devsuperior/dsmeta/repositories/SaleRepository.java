b248b1092d5b7213c39a1663d0b1a7c2a8d0f12e
Gabriel <gabrielkadaaj@yahoo.com.br>
2026-07-31 03:13:22 -0300

    feat: adiciona consulta de relatorio de vendas

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,23 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT obj FROM Sale obj "
+            + "WHERE obj.date BETWEEN :minDate AND :maxDate "
+            + "AND UPPER(obj.seller.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Sale> searchSales(
+            LocalDate minDate,
+            LocalDate maxDate,
+            String name,
+            Pageable pageable
+    );
 }
