3f01e2e64dea941cbfc4af894db99ef988214f65
Gabriel <gabrielkadaaj@yahoo.com.br>
2026-07-31 18:52:08 -0300

    feat: implementa servico de relatorio de vendas

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -4,6 +4,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,10 +19,40 @@ public class SaleService {
 
 	@Autowired
 	private SaleRepository repository;
-	
+
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
 	}
+
+	public Page<SaleReportDTO> getReport(
+			String minDate,
+			String maxDate,
+			String name,
+			Pageable pageable
+	) {
+		LocalDate today = LocalDate.now();
+
+		LocalDate max = maxDate == null || maxDate.isEmpty()
+				? today
+				: LocalDate.parse(maxDate);
+
+		LocalDate min = minDate == null || minDate.isEmpty()
+				? today.minusYears(1)
+				: LocalDate.parse(minDate);
+
+		String sellerName = name == null ? "" : name;
+
+		Page<Sale> result = repository.searchSales(min, max, sellerName, pageable);
+
+		return result.map(x -> new SaleReportDTO(
+				x.getId(),
+				x.getDate(),
+				x.getAmount(),
+				x.getSeller().getName()
+		));
+	}
+
+
 }
